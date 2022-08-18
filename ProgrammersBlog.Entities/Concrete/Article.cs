67474b9d6a50c57c88780df4145936728c1e3c22
osman28tr <osmantonbul2001@gmail.com>
2022-08-18 16:44:30 +0300

    Article Entity oluşturuldu.

---
+++ ProgrammersBlog.Entities/Concrete/Article.cs
@@ -0,0 +1,26 @@
+﻿using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Concrete
+{
+    public class Article:EntityBase,IEntity
+    {
+        public int CategoryId { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Thumbnail { get; set; }
+        public DateTime Date { get; set; }
+        public int ViewsCount { get; set; }
+        public int CommentCount { get; set; }
+        public string SeoAuthor { get; set; }
+        public string SeoDescription { get; set; }
+        public string SeoTags { get; set; }
+        public Category Category { get; set; }
+        public User User { get; set; }
+    }
+}
