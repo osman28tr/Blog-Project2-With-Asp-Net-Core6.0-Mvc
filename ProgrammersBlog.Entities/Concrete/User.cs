b31708e864913855f222aa026861a3f9c6253e11
osman28tr <osmantonbul2001@gmail.com>
2022-08-18 16:50:19 +0300

    Category,User ve Role Entity'leri oluşturuldu.

---
+++ ProgrammersBlog.Entities/Concrete/User.cs
@@ -0,0 +1,21 @@
+﻿using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Concrete
+{
+    public class User:EntityBase,IEntity
+    {
+        public int RoleId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public string UserName { get; set; }        
+        public Role Role { get; set; }
+        public ICollection<Article> Articles { get; set; }
+    }
+}
