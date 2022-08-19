6665c196d53857a5ad912f115669f66e61fe3e17
osman28tr <osmantonbul2001@gmail.com>
2022-08-19 15:04:03 +0300

    Repository Design Pattern-Data katmanında efrepository class'ları oluşturuldu.

---
+++ ProgrammersBlog.Data/Concrete/ArticleRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Data.Abstract;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Data.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Concrete
+{
+    public class ArticleRepository : EfEntityRepositoryBase<Article>, IArticleRepository
+    {
+        public ArticleRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ ProgrammersBlog.Data/Concrete/RoleRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Data.Abstract;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Data.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Concrete
+{
+    public class RoleRepository : EfEntityRepositoryBase<Role>, IRoleRepository
+    {
+        public RoleRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
