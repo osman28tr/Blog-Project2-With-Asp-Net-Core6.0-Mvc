41a0a285a64f8c3fbffaf3730fa00cbd9394255d
osman28tr <osmantonbul2001@gmail.com>
2022-08-20 20:59:08 +0300

    Role mapping işlemleri gerçekleştirildi.

---
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/RoleMap.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProgrammersBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Mappings
+{
+    public class RoleMap : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.Name).HasMaxLength(30);
+            builder.Property(x => x.Description).IsRequired();
+            builder.Property(x=>x.Description).HasMaxLength(250);
+            builder.Property(x => x.CreatedByName).IsRequired();
+            builder.Property(x => x.CreatedByName).HasMaxLength(50);
+            builder.Property(x => x.ModifiedByName).IsRequired();
+            builder.Property(x => x.ModifiedByName).HasMaxLength(50);
+            builder.Property(x => x.CreatedDate).IsRequired();
+            builder.Property(x => x.ModifiedDate).IsRequired();
+            builder.Property(x => x.IsDeleted).IsRequired();
+            builder.Property(x => x.IsActive).IsRequired();
+            builder.Property(x => x.Note).HasMaxLength(500);
+            builder.ToTable("Roles");
+        }
+    }
+}
