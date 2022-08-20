ce945e84795047f11d2db2a39cb7fe2cf1c95a49
osman28tr <osmantonbul2001@gmail.com>
2022-08-20 21:08:47 +0300

    User mapping işlemleri gerçekleştirildi.

---
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/UserMap.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProgrammersBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Mappings
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.Email).IsRequired();
+            builder.Property(x => x.Email).HasMaxLength(50);
+            builder.HasIndex(x => x.Email).IsUnique();
+            builder.Property(x => x.UserName).IsRequired();
+            builder.Property(x => x.UserName).HasMaxLength(20);
+            builder.HasIndex(x => x.UserName).IsUnique();
+            builder.Property(x => x.PasswordHash).IsRequired();
+            builder.Property(x => x.PasswordHash).HasColumnType("VARBINARY(500)");
+            builder.Property(x => x.Description).HasMaxLength(500);
+            builder.Property(x => x.FirstName).IsRequired();
+            builder.Property(x => x.FirstName).HasMaxLength(30);
+            builder.Property(x => x.LastName).IsRequired();
+            builder.Property(x => x.LastName).HasMaxLength(30);
+            builder.Property(x => x.Picture).IsRequired();
+            builder.Property(x => x.Picture).HasMaxLength(250);
+            builder.HasOne<Role>(x => x.Role).WithMany(x => x.Users).HasForeignKey(x => x.RoleId);
+            builder.Property(x => x.CreatedByName).IsRequired();
+            builder.Property(x => x.CreatedByName).HasMaxLength(50);
+            builder.Property(x => x.ModifiedByName).IsRequired();
+            builder.Property(x => x.ModifiedByName).HasMaxLength(50);
+            builder.Property(x => x.CreatedDate).IsRequired();
+            builder.Property(x => x.ModifiedDate).IsRequired();
+            builder.Property(x => x.IsDeleted).IsRequired();
+            builder.Property(x => x.IsActive).IsRequired();
+            builder.Property(x => x.Note).HasMaxLength(500);
+            builder.ToTable("Users");
+        }
+    }
+}
