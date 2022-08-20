9b3db59d105ba37764d16d98264c5768d79c71c9
osman28tr <osmantonbul2001@gmail.com>
2022-08-20 14:21:49 +0300

    Category Mapping işlemi gerçekleştirildi.

---
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/CategoryMap.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProgrammersBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Mappings
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x=>x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.Name).HasMaxLength(70);
+            builder.Property(x => x.Description).HasMaxLength(500);
+            builder.Property(x => x.CreatedByName).IsRequired();
+            builder.Property(x => x.CreatedByName).HasMaxLength(50);
+            builder.Property(x => x.ModifiedByName).IsRequired();
+            builder.Property(x => x.ModifiedByName).HasMaxLength(50);
+            builder.Property(x => x.CreatedDate).IsRequired();
+            builder.Property(x => x.ModifiedDate).IsRequired();
+            builder.Property(x => x.IsDeleted).IsRequired();
+            builder.Property(x => x.IsActive).IsRequired();
+            builder.Property(x => x.Note).HasMaxLength(500);
+            builder.ToTable("Categories");
+        }
+    }
+}
