2161a7dfe90b61f77955b95c30fa2da06697562b
osman28tr <osmantonbul2001@gmail.com>
2022-08-20 13:59:51 +0300

    Article Mapping işlemleri gerçekleştirildi.

---
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/ArticleMap.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProgrammersBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Mappings
+{
+    public class ArticleMap : IEntityTypeConfiguration<Article>
+    {
+        public void Configure(EntityTypeBuilder<Article> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.Title).HasMaxLength(100);
+            builder.Property(x => x.Title).IsRequired(true);
+            builder.Property(x => x.Content).IsRequired(true);
+            builder.Property(x => x.Content).HasColumnType("NVARCHAR(MAX)");
+            builder.Property(x => x.Date).IsRequired();
+            builder.Property(x=>x.SeoAuthor).IsRequired();
+            builder.Property(x => x.SeoAuthor).HasMaxLength(50);
+            builder.Property(x=>x.SeoDescription).HasMaxLength(150);
+            builder.Property(x => x.SeoDescription).IsRequired();
+            builder.Property(x => x.SeoTags).IsRequired();
+            builder.Property(x => x.SeoTags).HasMaxLength(70);
+            builder.Property(x => x.ViewsCount).IsRequired();
+            builder.Property(x => x.CommentCount).IsRequired();
+            builder.Property(x => x.Thumbnail).IsRequired();
+            builder.Property(x => x.Thumbnail).HasMaxLength(250);
+            builder.Property(x=>x.CreatedByName).IsRequired();
+            builder.Property(x=>x.CreatedByName).HasMaxLength(50);
+            builder.Property(x => x.ModifiedByName).IsRequired();
+            builder.Property(x => x.ModifiedByName).HasMaxLength(50);
+            builder.Property(x => x.CreatedDate).IsRequired();
+            builder.Property(x => x.ModifiedDate).IsRequired();
+            builder.Property(x => x.IsDeleted).IsRequired();
+            builder.Property(x => x.IsActive).IsRequired();
+            builder.Property(x => x.Note).HasMaxLength(500);
+            builder.HasOne<Category>(x => x.Category).WithMany(y => y.Articles).HasForeignKey(z => z.CategoryId);
+            builder.HasOne<User>(x => x.User).WithMany(y => y.Articles).HasForeignKey(z => z.UserId);
+            builder.ToTable("Articles");
+        }
+    }
+}
