74ad8cf7420efdf52fca1a0b1545b19ea0f9b11d
osman28tr <osmantonbul2001@gmail.com>
2022-08-26 15:16:20 +0300

    CategoryAddDto sınıfı eklendi.

--- ProgrammersBlog.Entities/Dtos/CategoryAddDto.cs
+++ ProgrammersBlog.Entities/Dtos/CategoryAddDto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,21 @@ namespace ProgrammersBlog.Entities.Dtos
 {
     public class CategoryAddDto
     {
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage ="{0} boş geçilemez")]
+        [MaxLength(70,ErrorMessage ="{0} {1} karakterden büyük olamaz.")]
+        [MinLength(3,ErrorMessage ="{0} {1} karakterden az olamaz.")]
+        public string Name { get; set; }
+        [DisplayName("Kategori Açıklaması")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olamaz.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olamaz.")]
+        public string Description { get; set; }
+        [DisplayName("Kategori Özel Not Alanı")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olamaz.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olamaz.")]
+        public string Note { get; set; }
+        [DisplayName("Aktif Mi?")]
+        [Required(ErrorMessage = "{0} boş geçilemez")]
+        public bool IsActive { get; set; }
     }
 }
