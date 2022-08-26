5d3951c20e009bf28f5694ede4340bb15c297ddf
osman28tr <osmantonbul2001@gmail.com>
2022-08-26 15:39:40 +0300

    CategoryUpdateDto sınıfı eklendi.

--- ProgrammersBlog.Entities/Dtos/CategoryAddDto.cs
+++ ProgrammersBlog.Entities/Dtos/CategoryAddDto.cs
@@ -11,20 +11,20 @@ namespace ProgrammersBlog.Entities.Dtos
     public class CategoryAddDto
     {
         [DisplayName("Kategori Adı")]
-        [Required(ErrorMessage ="{0} boş geçilemez")]
-        [MaxLength(70,ErrorMessage ="{0} {1} karakterden büyük olamaz.")]
-        [MinLength(3,ErrorMessage ="{0} {1} karakterden az olamaz.")]
+        [Required(ErrorMessage ="{0} boş geçilmemelidir.")]
+        [MaxLength(70,ErrorMessage ="{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(3,ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
         public string Name { get; set; }
         [DisplayName("Kategori Açıklaması")]
-        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olamaz.")]
-        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olamaz.")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
         public string Description { get; set; }
         [DisplayName("Kategori Özel Not Alanı")]
-        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olamaz.")]
-        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olamaz.")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
         public string Note { get; set; }
         [DisplayName("Aktif Mi?")]
-        [Required(ErrorMessage = "{0} boş geçilemez")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
         public bool IsActive { get; set; }
     }
 }
