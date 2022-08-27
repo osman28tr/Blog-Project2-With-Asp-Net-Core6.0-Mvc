913477f0e5717056277e4ed724ffcd58f9b7a5db
osman28tr <osmantonbul2001@gmail.com>
2022-08-27 13:57:21 +0300

    IArticle interface'i ve ilgili dto'ları oluşturuldu.

---
+++ ProgrammersBlog.Services/Abstract/IArticleService.cs
@@ -0,0 +1,24 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Services.Abstract
+{
+    public interface IArticleService
+    {
+        Task<IDataResult<ArticleDto>> Get(int articleId);
+        Task<IDataResult<IList<ArticleListDto>>> GetAll();
+        Task<IDataResult<IList<ArticleListDto>>> GetAllByNonDeleted();
+        Task<IDataResult<IList<ArticleListDto>>> GetAllByNonDeletedAndActive();
+        Task<IDataResult<IList<ArticleListDto>>> GetAllByCategory(int categoryId);
+        Task<IResult> Add(ArticleAddDto articleAddDto, string createdByName);
+        Task<IResult> Update(ArticleUpdateDto articleUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int articleId, string modifiedByName);
+        Task<IResult> HardDelete(int articleId);
+    }
+}
