47dda53f00790ae5d6cfdfab7173b67bb63cf8ee
osman28tr <osmantonbul2001@gmail.com>
2022-08-27 14:03:59 +0300

    Get Dto sınıfı işlemleri gerçekleştirildi.

--- ProgrammersBlog.Entities/Dtos/ArticleDto.cs
+++ ProgrammersBlog.Entities/Dtos/ArticleDto.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Entities.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,8 @@ using System.Threading.Tasks;
 
 namespace ProgrammersBlog.Entities.Dtos
 {
-    public class ArticleDto
+    public class ArticleDto:DtoGetBase
     {
+        public Article Article { get; set; }
     }
 }
---
+++ ProgrammersBlog.Shared/Entities/Abstract/DtoGetBase.cs
@@ -0,0 +1,14 @@
+﻿using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Entities.Abstract
+{
+    public abstract class DtoGetBase
+    {
+        public virtual ResultStatus ResultStatus { get; set; }
+    }
+}
