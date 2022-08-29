9d747ff3466e383689805450bea28bdb566237e5
osman28tr <osmantonbul2001@gmail.com>
2022-08-29 23:49:42 +0300

    ServiceCollection eklendi ve gerekli konfigürasyon ayarı yapıldı.

---
+++ ProgrammersBlog.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProgrammersBlog.Mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
--- ProgrammersBlog.Mvc/Program.cs
+++ ProgrammersBlog.Mvc/Program.cs
@@ -1,6 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using ProgrammersBlog.Services.Extensions;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+builder.Services.LoadMyServices();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
