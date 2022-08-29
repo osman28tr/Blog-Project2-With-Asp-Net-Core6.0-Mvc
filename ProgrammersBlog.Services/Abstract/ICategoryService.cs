3f231ecc32c1b694afdbcc24c883a79a055a2682
osman28tr <osmantonbul2001@gmail.com>
2022-08-29 23:25:37 +0300

    CategoryManager sınfı dto ve mapping işlemlerine göre yeniden revize edildi.

--- ProgrammersBlog.Services/Abstract/ICategoryService.cs
+++ ProgrammersBlog.Services/Abstract/ICategoryService.cs
@@ -11,9 +11,10 @@ namespace ProgrammersBlog.Services.Abstract
 {
     public interface ICategoryService
     {
-        Task<IDataResult<Category>> Get(int categoryId);
-        Task<IDataResult<IList<Category>>> GetAll();
-        Task<IDataResult<IList<Category>>> GetAllByNonDeleted();
+        Task<IDataResult<CategoryDto>> Get(int categoryId);
+        Task<IDataResult<CategoryListDto>> GetAll();
+        Task<IDataResult<CategoryListDto>> GetAllByNonDeleted();
+        Task<IDataResult<CategoryListDto>> GetAllByNonDeletedAndActive();
         Task<IResult> Add(CategoryAddDto categoryAddDto, string createdByName);
         Task<IResult> Update(CategoryUpdateDto categoryUpdateDto,string modifiedByName);
         Task<IResult> Delete(int categoryId, string modifiedByName);
