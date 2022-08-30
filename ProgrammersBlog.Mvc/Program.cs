174e44fce0b39c23b10c0e5e0f62ef9e215e33ef
osman28tr <osmantonbul2001@gmail.com>
2022-08-30 11:53:47 +0300

    program.cs dosyasında automapper ve razorruntimecompilation uygulamaları eklendi ve konfigürasyon ayarları yapıldı.

--- ProgrammersBlog.Mvc/Program.cs
+++ ProgrammersBlog.Mvc/Program.cs
@@ -3,15 +3,17 @@ using ProgrammersBlog.Services.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
+builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.LoadMyServices();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+app.UseStatusCodePages();
 app.UseStaticFiles();
 
 app.UseRouting();
