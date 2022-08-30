f35b78b223f40d96520e7819e9415890774a68a9
osman28tr <osmantonbul2001@gmail.com>
2022-08-30 12:10:03 +0300

    Admin area oluşturuldu ve konfigürasyon ayarı yapıldı.

--- ProgrammersBlog.Mvc/Program.cs
+++ ProgrammersBlog.Mvc/Program.cs
@@ -24,4 +24,11 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
 app.Run();
