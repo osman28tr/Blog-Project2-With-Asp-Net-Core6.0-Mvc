e831a191dd4b5de7cfe81b89172ac0cfc80a5f0b
osman28tr <osmantonbul2001@gmail.com>
2022-08-31 16:18:42 +0300

    Admin kısmında kategorilerin datatable'da listelenmesi işlemi gerçekleştirildi.

---
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var result = await _categoryService.GetAll();
+            if (result.ResultStatus==ResultStatus.Success)
+            {
+                return View(result.Data);
+            }
+            return View();
+        }
+    }
+}
--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/HomeController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/HomeController.cs
@@ -3,6 +3,7 @@
 namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
     public class HomeController : Controller
     {
         public IActionResult AdminIndex()
