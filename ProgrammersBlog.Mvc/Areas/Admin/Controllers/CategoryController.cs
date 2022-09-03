e1c0d2da749b1ad9f2473cdd0da3a7a22b483ed9
osman28tr <osmantonbul2001@gmail.com>
2022-09-03 14:35:03 +0300

    admin kısmında categoryadd partial view'i oluşturuldu ve modal içerisinde jquery ile görüntülendi.

--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -18,5 +18,9 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
             var result = await _categoryService.GetAll();
             return View(result.Data);
         }
+        public IActionResult Add()
+        {
+            return PartialView("_CategoryAddPartial");
+        }
     }
 }
