b31c0ca20d6d7731e3aff84d1f182f7e5f93a5b2
osman28tr <osmantonbul2001@gmail.com>
2022-09-07 14:45:10 +0300

    Ajax post ile ekleme işleminde category için validasyon mesajlarını toastr içerisinde gösterildi.

--- ProgrammersBlog.Mvc/wwwroot/js/site.js
+++ ProgrammersBlog.Mvc/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function convertFirstLetterToUpperCase(text) {
+    return text.chartAt(0).toUpperCase() + text.slice(1);
+}
+function convertToShortDate(dateString) {
+    const shortDate = new Date(dateString).toLocaleDateString('en-US');
+    return shortDate;
+}
