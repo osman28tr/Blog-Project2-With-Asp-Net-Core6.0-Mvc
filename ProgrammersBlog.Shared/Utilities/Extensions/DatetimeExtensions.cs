bd5dac567316fed752b7c0676f3ad9618d50a67f
osman28tr <osmantonbul2001@gmail.com>
2022-09-14 20:49:52 +0300

    User resim yükleme işlemi için datetime extensions sınıfı eklendi.

---
+++ ProgrammersBlog.Shared/Utilities/Extensions/DatetimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Utilities.Extensions
+{
+    public static class DatetimeExtensions
+    {
+        public static string FullDateAndTimeStringWithUnderscore(this DateTime dateTime)
+        {
+            return $"{dateTime.Millisecond}_{dateTime.Second}_{dateTime.Minute}_{dateTime.Hour}_{dateTime.Day}_{dateTime.Month}_{dateTime.Year}";
+        }
+    }
+}
