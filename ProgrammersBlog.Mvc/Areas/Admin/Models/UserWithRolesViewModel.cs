cfaf7ab31c2c17524ec92eb2063a249fa027f09e
osman28tr <osmantonbul2001@gmail.com>
2022-09-22 23:35:29 +0300

    Admin sidebar menüsü viewcomponent olarak revize edildi.

---
+++ ProgrammersBlog.Mvc/Areas/Admin/Models/UserWithRolesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Models
+{
+	public class UserWithRolesViewModel
+	{
+		public User User { get; set; }
+		public IList<string> Roles { get; set; }
+	}
+}
