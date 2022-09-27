ea9fc28de12122dff61692814285531b56ac885f
osman28tr <osmantonbul2001@gmail.com>
2022-09-27 19:31:13 +0300

    Dashboardviewmodel oluşturuldu ve veriler ile dolduruldu.

--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/HomeController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/HomeController.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Mvc.Areas.Admin.Models;
+using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
 
 namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
 {
@@ -8,9 +14,36 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
     [Authorize(Roles = "Admin,Editor")]
     public class HomeController : Controller
     {
-        public IActionResult AdminIndex()
+        private readonly ICategoryService _categoryService;
+        private readonly IArticleService _articleService;
+        private readonly ICommentService _commentService;
+        private readonly UserManager<User> _userManager;
+        public HomeController(ICategoryService categoryService, IArticleService articleService, ICommentService commentService, UserManager<User> userManager)
         {
-            return View();
+            _categoryService = categoryService;
+            _articleService = articleService;
+            _commentService = commentService;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> AdminIndex()
+        {
+            var categoriesCountResult = await _categoryService.CountByIsDeleted();
+            var articlesCountResult = await _articleService.CountByIsDeleted();
+            var commentsCountResult = await _commentService.CountByIsDeleted();
+            var usersCount = await _userManager.Users.CountAsync();
+            var articleResult = await _articleService.GetAll();
+            if (categoriesCountResult.ResultStatus == ResultStatus.Success && articlesCountResult.ResultStatus == ResultStatus.Success && commentsCountResult.ResultStatus == ResultStatus.Success && usersCount > -1)
+            {
+                return View(new DashboardViewModel
+                {
+                    CategoriesCount = categoriesCountResult.Data,
+                    ArticlesCount = articlesCountResult.Data,
+                    CommentsCount = commentsCountResult.Data,
+                    UsersCount = usersCount,
+                    Articles = articleResult.Data
+                });
+            }
+            return NotFound();
         }
     }
 }
---
+++ ProgrammersBlog.Mvc/Areas/Admin/Models/DashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Entities.Dtos;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Models
+{
+    public class DashboardViewModel
+    {
+        public int CategoriesCount { get; set; }
+        public int ArticlesCount { get; set; }
+        public int CommentsCount { get; set; }
+        public int UsersCount { get; set; }
+        public ArticleListDto Articles { get; set; }
+    }
+}
